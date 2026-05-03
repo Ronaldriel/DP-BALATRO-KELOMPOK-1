63b8fa4c4f812b88aa447694eea804eec3071c3a
IFTxz <fritzputra.01@gmail.com>
2026-05-03 22:30:28 +0700

    update hand

--- Hand.h
+++ Hand.h
@@ -4,8 +4,8 @@
 struct Hand {
     int value ;
     Hand() {
-        static std::mt19937 rng(std::random_device{}());
-        static std::uniform_int_distribution<int> dist(0, 12);
-        value = dist(rng);
+        //static std::mt19937 rng(std::random_device{}());
+        //static std::uniform_int_distribution<int> dist(0, 12);
+        value = 5;
     }
 };
--- RewardRule.cpp
+++ RewardRule.cpp
@@ -2,10 +2,12 @@
 #include "RewardRule.h"
 int RewardRule::earnMoney(bool win, int score)
 {
+	int resultreward = score/2;
 	std::cout << "Calculating reward...\n";
 	if (win)
-	{
-		return score;
+	{ 
+		
+		return resultreward;
 
 	}
 	return 0;
